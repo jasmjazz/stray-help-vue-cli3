d288d9e8772ee71d3ecf13af11312b6b82966845
jasmjazz <house5324@gmail.com>
2021-02-15 08:34:12 +0800

    0214

--- src/components/front/Nav.vue
+++ src/components/front/Nav.vue
@@ -3,7 +3,8 @@
     <div class="wrap">
       <ul class="nav">
         <li class="nav-item home">
-          <router-link class="nav-link" :to="{ name: 'Home' }">Strap-Help</router-link>
+          <router-link class="nav-link" :to="{ name: 'Home' }" exact
+          active-class="active">Strap-Help</router-link>
         </li>
         <li class="nav-item">
           <router-link class="nav-link" :to="{ name: 'About' }">關於我們</router-link>
@@ -26,9 +27,12 @@ export default {
 .nav-link {
   font-size: 17px;
   font-weight: 560;
-  color: #616161;
+  color: #8e8e8e;
 }
 .home {
   font-family: Papyrus;
+}
+.active {
+  color: #333;
 }
 </style>
